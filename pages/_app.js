42bc92c0d9d1900a61d916bf38795e5b6a9a4009
growninfant <giuseppedigiorno@yahoo.it>
2022-01-11 16:32:17 +0100

    fixed

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,19 @@ import Script from 'next/script'
 function MyApp({ Component, pageProps }) {
   return(
     <>
+       <Script
+      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+      strategy='afterInteractive'
+    />
+       <Script id="google-analytics" strategy="afterInteractive">
+        {`
+         window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments);}
+         gtag('js', new Date());
+       
+         gtag('config', '${process.env.GOOGLE_ANALYTICS}');
+        `}
+      </Script>
       <Component {...pageProps} />
     </>
   ) 
