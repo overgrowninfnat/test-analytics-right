b5bda5c599b3073825819067efb73472b456b5b8
growninfant <giuseppedigiorno@yahoo.it>
2022-01-11 16:18:33 +0100

    analytics code added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <>
+    <Script
+      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+      strategy='afterInteractive'
+    />
+       <Script id="google-analytics" strategy="afterInteractive">
+        {`
+         window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments);}
+         gtag('js', new Date());
+       
+         gtag('config', '${process.env.GOOGLE_ANALYTICS}');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  ) 
 }
 
 export default MyApp
